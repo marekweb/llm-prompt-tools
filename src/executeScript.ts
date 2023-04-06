3ed3f8ef0ccaf38ae467ea7832f9bec090d936af
Marek <marekz@gmail.com>
2023-04-05 22:13:58 -0400

    Handle errors

--- src/executeScript.ts
+++ src/executeScript.ts
@@ -4,7 +4,8 @@ import { CapturingLogger } from "./CapturingLogger";
 export const executeScript = (
   script: string,
   context?: Record<string, unknown>
-) => {
+): { output: string; error: string | undefined } => {
+  let error: string | undefined;
   const capturingLogger = new CapturingLogger();
   const vm = new VM({
     timeout: 1000,
@@ -12,10 +13,15 @@ export const executeScript = (
   });
 
   // run() returns the evalued value of the last expression in the script
-  const result = vm.run(script);
+  let result: unknown;
+  try {
+    result = vm.run(script);
+  } catch (e) {
+    error = String(e);
+  }
   let output = capturingLogger.getOutput();
   if (result !== undefined) {
     output += result + "\n";
   }
-  return output;
+  return { output, error };
 };
