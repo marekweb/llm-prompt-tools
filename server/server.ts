49e405d55fb5ada19c254692fb1fc48d824f6df0
Marek <marekz@gmail.com>
2023-04-12 00:41:28 -0400

    Update server and socket messages

--- server/server.ts
+++ server/server.ts
@@ -1,149 +1,39 @@
 import express from "express";
-import { config } from "dotenv";
 import { Server as WebSocketServer, WebSocket } from "ws";
-import { createServer } from "http";
+import { Server, createServer } from "http";
 import { AddressInfo } from "net";
-import { v4 as uuidv4 } from "uuid";
+import { Message } from "../src/Message";
+import {
+  SocketMessageFromClient,
+  SocketMessageFromServer,
+  parseSocketMessageFromClient,
+} from "./socket-messages";
 
-interface SocketMessage {
-  type: string;
-  id?: string;
-  content?: string;
-}
-
-interface ConnectionContext {
-  id: string;
-  socket: WebSocket[];
+interface SocketConnection {
+  conversationId?: string;
+  socket: WebSocket;
   lastActive: number;
-  conversation: string; // TODO
 }
 
-// This is a type guard for string | undefined | null
-function isString(value: unknown): value is string | undefined {
-  return typeof value === "string" || value === undefined;
+type HandleMessageFromClient = (message: SocketMessageFromClient) => void;
+type SendMessageToClient = (
+  conversationId: string,
+  message: SocketMessageFromServer
+) => void;
+
+interface CreateServerOptions {
+  port: number;
+  createConversation: (agentId: string, message: string) => Promise<string>;
+  getConversation: (conversationId: string) => Promise<Message[]>;
+  handleMessage: HandleMessageFromClient;
 }
 
-function parseMessageAsSocketMessage(jsonString: string): SocketMessage | null {
-  try {
-    const parsed = JSON.parse(jsonString);
-
-    // Check if 'type' is a string and exists
-    if (typeof parsed.type !== "string") {
-      return null;
-    }
-
-    // Check if 'id' is a string, undefined, or not present
-    if (parsed.id !== undefined && !isString(parsed.id)) {
-      return null;
-    }
-
-    // Check if 'content' is a string, undefined, or not present
-    if (parsed.content !== undefined && !isString(parsed.content)) {
-      return null;
-    }
-
-    return parsed as SocketMessage;
-  } catch (error) {
-    return null;
-  }
+interface ConversationServer {
+  send: SendMessageToClient;
+  app: express.Application;
+  server: Server;
+  wss: WebSocketServer;
 }
-
-config();
-const app = express();
-const server = createServer(app);
-const PORT = process.env.PORT || 3000;
-
-// Websocket server
-const wss = new WebSocketServer({ server });
-
-const connections = new Map<string, ConnectionContext>();
-
-function send(id: string, message: string) {
-  const connectionContext = connections.get(id);
-  if (!connectionContext) {
-    console.warn(`No connection found for id ${id}`);
-    return;
-  }
-
-  connectionContext.socket.forEach((socket) =>
-    socket.send(
-      JSON.stringify({
-        id,
-        type: "message",
-        content: message,
-      })
-    )
-  );
-}
-
-function handleMessage(id: string, message: string) {
-  console.log(`Received message from ${id}: ${message}`);
-}
-
-wss.on("connection", (ws) => {
-  // Handle incoming message
-  ws.on("message", (message) => {
-    const data = parseMessageAsSocketMessage(message.toString());
-    if (!data) {
-      return;
-    }
-    switch (data.type) {
-      case "connect":
-        const id = uuidv4();
-        connections.set(id, {
-          id,
-          socket: [ws],
-          lastActive: Date.now(),
-          conversation: "",
-        });
-        ws.send(
-          JSON.stringify({
-            id,
-            type: "connect",
-            content: "Connected",
-          })
-        );
-        break;
-      case "reconnect":
-        if (!data.id) {
-          break;
-        }
-        const connectionContext = connections.get(data.id);
-        if (!connectionContext) {
-          break;
-        }
-        connectionContext.socket.push(ws);
-        connectionContext.lastActive = Date.now();
-        ws.send(
-          JSON.stringify({
-            id: data.id,
-            type: "reconnect",
-            content: "Reconnected",
-          })
-        );
-        break;
-
-      case "message":
-        if (!data.id || !data.content) {
-          break;
-        }
-        handleMessage(data.id, data.content);
-        break;
-
-      default:
-        // TODO: Handle unsupported message type
-        console.warn(`Unsupported message type: ${data.type}`);
-        break;
-    }
-  });
-
-  // Handle client disconnection
-  ws.on("close", () => {
-    connections.forEach((value, key) => {
-      removeItem(value.socket, ws);
-    });
-  });
-});
 
 function removeItem<T>(array: T[], item: T) {
   const index = array.indexOf(item);
@@ -152,13 +42,115 @@ function removeItem<T>(array: T[], item: T) {
   }
 }
 
-// Serve index.html
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+function sendMessage(socket: WebSocket, message: SocketMessageFromServer) {
+  socket.send(JSON.stringify(message));
+}
 
-// Start server
-server.listen(PORT, () => {
-  const address = server.address() as AddressInfo;
-  console.log(`WebSocket server running on http://localhost:${address.port}`);
-});
+export function createConversationServer(
+  options: CreateServerOptions
+): ConversationServer {
+  const app = express();
+  const server = createServer(app);
+
+  const connections: SocketConnection[] = [];
+
+  // Websocket server
+  const wss = new WebSocketServer({ server });
+
+  wss.on("connection", (ws) => {
+    // Handle incoming message
+
+    const socketConnectionRecord: SocketConnection = {
+      socket: ws,
+      lastActive: Date.now(),
+    };
+    connections.push(socketConnectionRecord);
+
+    ws.on("message", (rawMessage) => {
+      socketConnectionRecord.lastActive = Date.now();
+      const message = parseSocketMessageFromClient(rawMessage.toString());
+      if (!message) {
+        sendMessage(ws, {
+          type: "message",
+          state: "error",
+          content: "error: bad message",
+        });
+        return;
+      }
+
+      switch (message.type) {
+        case "message":
+          if (message.agentId) {
+            const conversationId = await options.createConversation(
+              message.agentId,
+              message.content
+            );
+            socketConnectionRecord.conversationId = conversationId;
+            // TODO: We could reply something here, like the
+            // conversation ID, but instead we wait until there's an
+            // actual message to send.
+            return;
+          }
+
+          if (!message.conversationId) {
+            sendMessage(ws, {
+              type: "message",
+              state: "error",
+              content: "error: no conversation ID",
+            });
+            return;
+          }
+      }
+    });
+    // Handle client disconnection
+    ws.on("close", () => {
+      // Remove form connections array
+      removeItem(connections, socketConnectionRecord);
+    });
+  });
+
+  // Serve index.html
+  app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+  });
+
+  app.get("/api/conversations/:id", (req, res) => {
+    // Returns the history of a conversation.
+    const id = req.params.id;
+    const conversation = options.getConversation(id);
+    if (!conversation) {
+      res.status(404).send("Conversation not found");
+      return;
+    }
+    res.send({ conversation });
+  });
+
+  app.get("/api/conversations", (req, res) => {
+    // TODO: implement
+    res.send([]);
+  });
+
+  // Start server
+  server.listen(options.port, () => {
+    const address = server.address() as AddressInfo;
+    console.log(
+      `WebSocket server running on ${address.family}://${address.address}:${address.port}`
+    );
+  });
+
+  function send(conversationId: string, message: SocketMessageFromServer) {
+    const connectionsForConversation = connections.filter(
+      (c) => c.conversationId === conversationId
+    );
+    connectionsForConversation.forEach((connection) => {
+      sendMessage(connection.socket, message);
+    });
+  }
+
+  return {
+    send,
+    app,
+    server,
+    wss,
+  };
+}
--- server/socket-messages.ts
+++ server/socket-messages.ts
@@ -1,36 +1,49 @@
 export interface SocketMessageFromClient {
-    type: "message" | "conversation";
-    content: string;
+  type: "message";
+  conversationId?: string;
+  agentId?: string;
+  content: string;
 }
 
 export interface SocketMessageFromServer {
-    type: 'message' | 'state'
-    conversationId: string;
-    content: string;
+  type: "message";
+  conversationId?: string;
+  state?: string;
+  content?: string;
 }
 
+function isSocketMessageFromClient(
+  object: unknown
+): object is SocketMessageFromClient {
+  if (typeof object !== "object" || object === null) {
+    return false;
+  }
 
-function isSocketMessageFromClient(obj: unknown): obj is SocketMessageFromClient {
-    return (
-        typeof obj === "object" &&
-        obj !== null &&
-        "type" in obj &&
-        ("message" === (obj as { type?: unknown }).type || "conversation" === (obj as { type?: unknown }).type) &&
-        "content" in obj &&
-        typeof (obj as { content?: unknown }).content === "string"
-    );
+  const { type, content, conversationId, agentId } =
+    object as Partial<SocketMessageFromClient>;
+
+  const isValidType = type === "message";
+  const isValidContent = typeof content === "string";
+  const isValidConversationId =
+    typeof conversationId === "string" || conversationId === undefined;
+  const isValidAgentId = typeof agentId === "string" || agentId === undefined;
+
+  return (
+    isValidType && isValidContent && isValidConversationId && isValidAgentId
+  );
 }
 
+export function parseSocketMessageFromClient(
+  jsonString: string
+): SocketMessageFromClient | null {
+  if (!jsonString || !jsonString.trim()) {
+    return null;
+  }
 
-export function parseSocketMessageFromClient(jsonString: string): SocketMessageFromClient | null {
-    if (!jsonString || !jsonString.trim()) {
-        return null;
-    }
-
-    try {
-        const parsedObject = JSON.parse(jsonString.trim());
-        return isSocketMessageFromClient(parsedObject) ? parsedObject : null;
-    } catch (error) {
-        return null;
-    }
+  try {
+    const parsedObject = JSON.parse(jsonString.trim());
+    return isSocketMessageFromClient(parsedObject) ? parsedObject : null;
+  } catch (error) {
+    return null;
+  }
 }
